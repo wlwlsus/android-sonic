8e702a51b58774ae6e989d7f38218570d34b8a78
swj <159632_@naver.com>
2021-10-19 23:28:57 +0900

    add:toolbar

--- app/src/main/java/com/dontsc/sonicapplication/presentation/view/LoadingActivity.kt
+++ app/src/main/java/com/dontsc/sonicapplication/presentation/view/LoadingActivity.kt
@@ -1,14 +1,52 @@
 package com.dontsc.sonicapplication.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.dontsc.sonicapplication.R
 
-class LoadingActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dontsc.sonicapplication.R
+import com.dontsc.sonicapplication.databinding.ActivityLoadingBinding
+import com.dontsc.sonicapplication.presentation.view.base.UtilityBase
+
+
+class LoadingActivity :
+    UtilityBase.BaseAppCompatActivity<ActivityLoadingBinding>(R.layout.activity_loading) {
+
+    override fun ActivityLoadingBinding.onCreate() {
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//
+//        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        setSupportActionBar(binding.toolbar)
+
+        binding.apply {
+            toolbarAddress.setOnClickListener {
+                Toast.makeText(this@LoadingActivity, "Go to Address Page", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            toolbarGithub.setOnClickListener {
+                Toast.makeText(this@LoadingActivity, "Go to Github Page", Toast.LENGTH_SHORT).show()
+            }
+
+            toolbarSearch.setOnClickListener {
+                Toast.makeText(this@LoadingActivity, "Go to Search Page", Toast.LENGTH_SHORT).show()
+            }
+
+            toolbarMenu.setOnClickListener {
+                Toast.makeText(this@LoadingActivity, "Go to Menu Page", Toast.LENGTH_SHORT).show()
+            }
+
+            toolbarAlim.setOnClickListener {
+                Toast.makeText(this@LoadingActivity, "Go to Alim Page", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 
     override fun onBackPressed() {
         super.onBackPressed()
